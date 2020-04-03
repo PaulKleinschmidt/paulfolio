c0c59cd7c3aef5cb8335c49ee2e90276ddda1df9
Paul Kleinschmidt <paul.kleinschmidt1@gmail.com>
2020-04-03 09:56:06 -0700

    Updates

---
+++ precache-manifest.1d30d1e97d52e0567fcee2673fb132a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f8035391898c6ba4fd933804cae55f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "264867140c5deb4fc12a",
+    "url": "/static/css/main.1a89167a.chunk.css"
+  },
+  {
+    "revision": "bfcbace79b5e8ef7a285",
+    "url": "/static/js/2.2c76b389.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.2c76b389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "264867140c5deb4fc12a",
+    "url": "/static/js/main.c8d18143.chunk.js"
+  },
+  {
+    "revision": "736b3dccb2ef0498a0e3",
+    "url": "/static/js/runtime-main.2558b9be.js"
+  },
+  {
+    "revision": "3ba09168aeb16af7391ecad14fcbd184",
+    "url": "/static/media/chicken.3ba09168.png"
+  },
+  {
+    "revision": "782af66e3fb866a4fcb643619d6769fa",
+    "url": "/static/media/sqnce.782af66e.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/www.paulkleinschmidt.me/precache-manifest.f232bd495a2706c938a345287f1eaa90.js"
+  "/precache-manifest.1d30d1e97d52e0567fcee2673fb132a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.paulkleinschmidt.me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
