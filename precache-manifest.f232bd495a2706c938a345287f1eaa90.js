201970cc868879933eab5de9fb37bb18da1f8512
Paul Kleinschmidt <paul.kleinschmidt1@gmail.com>
2020-04-03 09:55:03 -0700

    Updates

---
+++ precache-manifest.f232bd495a2706c938a345287f1eaa90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31f43683728f533bae12b8204a29b79",
+    "url": "/www.paulkleinschmidt.me/index.html"
+  },
+  {
+    "revision": "6de3ec065e096d0c0da4",
+    "url": "/www.paulkleinschmidt.me/static/css/main.1a89167a.chunk.css"
+  },
+  {
+    "revision": "95ad174063f596aac3eb",
+    "url": "/www.paulkleinschmidt.me/static/js/2.0e5c516d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/www.paulkleinschmidt.me/static/js/2.0e5c516d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6de3ec065e096d0c0da4",
+    "url": "/www.paulkleinschmidt.me/static/js/main.16749986.chunk.js"
+  },
+  {
+    "revision": "1ccdf3f38493ff88e11a",
+    "url": "/www.paulkleinschmidt.me/static/js/runtime-main.3a59417a.js"
+  },
+  {
+    "revision": "3ba09168aeb16af7391ecad14fcbd184",
+    "url": "/www.paulkleinschmidt.me/static/media/chicken.3ba09168.png"
+  },
+  {
+    "revision": "782af66e3fb866a4fcb643619d6769fa",
+    "url": "/www.paulkleinschmidt.me/static/media/sqnce.782af66e.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/paulfolio/precache-manifest.e0a28292daa992deffbac109807e3ce8.js"
+  "/www.paulkleinschmidt.me/precache-manifest.f232bd495a2706c938a345287f1eaa90.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paulfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.paulkleinschmidt.me/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
