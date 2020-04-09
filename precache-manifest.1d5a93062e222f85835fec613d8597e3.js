9eec5d39eb9dfb8ba1f6e5d8ef764c4615a2cdc2
Paul Kleinschmidt <paul.kleinschmidt1@gmail.com>
2020-04-09 15:55:55 -0700

    Updates

--- precache-manifest.1d5a93062e222f85835fec613d8597e3.js
+++ precache-manifest.1d5a93062e222f85835fec613d8597e3.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "19f8035391898c6ba4fd933804cae55f",
+    "revision": "2bd482ff15bcef57a48f51ffc40eb253",
     "url": "/index.html"
   },
   {
-    "revision": "264867140c5deb4fc12a",
+    "revision": "736359e14a76492847c8",
     "url": "/static/css/main.1a89167a.chunk.css"
   },
   {
-    "revision": "bfcbace79b5e8ef7a285",
-    "url": "/static/js/2.2c76b389.chunk.js"
+    "revision": "7bee03fc24632f5e52f7",
+    "url": "/static/js/2.1f7a0f48.chunk.js"
   },
   {
     "revision": "f231859d6585c4cd5f80c344783ed269",
-    "url": "/static/js/2.2c76b389.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.1f7a0f48.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "264867140c5deb4fc12a",
-    "url": "/static/js/main.c8d18143.chunk.js"
+    "revision": "736359e14a76492847c8",
+    "url": "/static/js/main.0afba285.chunk.js"
   },
   {
     "revision": "736b3dccb2ef0498a0e3",
     "url": "/static/js/runtime-main.2558b9be.js"
+  },
+  {
+    "revision": "25cd56e86aa31e94ef60ac02d4cec09c",
+    "url": "/static/media/baba.25cd56e8.png"
   },
   {
     "revision": "3ba09168aeb16af7391ecad14fcbd184",
